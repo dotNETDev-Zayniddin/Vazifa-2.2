6189b83849264fb4d6de529bec7efd71d44e06e7
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-03 22:16:09 +0500

    Vazifa: Project Upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Arifmetik progressiya yig'indisi formulasidan foydalanildi
+using System;
+
+    namespace Vazifa
+    {
+        class Program{
+            static void Main(string[] args){
+                Console.Write("N raqamini kiriting: ");
+                int N = Convert.ToInt32(Console.ReadLine());
+
+                decimal natija = N*(1+N)/2;
+
+
+                Console.Write($"Natija: {natija}");
+            }
+        }
+
+    }
+
